e5932eddd7745a27bdcfe957650c2ab2505420bc
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-19 06:50:44 -0700

    0-go_printf.c

--- 0-go_printf.c
+++ 0-go_printf.c
@@ -23,14 +23,20 @@ int _printf(const char *format, ...)
 		if (format[x] == '%')
 		{
 			x++;
-			if (format[x] == 'c' || format[x] == 's' || format[x] == 's')
+			if (format[x] == 'c' || format[x] == 's')
 			{
 				pchars += finder(format[x])(arlist);
+			}
+			else if (format[x] == 'd' || format[x] == 'i')
+			{
+				int value = va_arg(arlist, int);
+
+				pchars += _putchar_int(value);
 			}
 			else
 			{
 				pchars += _putchar('%');
-				continue;
+				pchars += _putchar(format[x]);
 			}
 		}
 		else
@@ -40,4 +46,34 @@ int _printf(const char *format, ...)
 	}
 	va_end(arlist);
 	return (pchars);
+}
+/**
+ * _putchar_int - Prints an integer to the standard output.
+ * @n: The integer to be printed.
+ *
+ * This function takes an integer as input and recursively prints its digits
+ * one by one to the standard output. If the integer is negative, it first
+ * prints a '-' sign before printing the absolute value of the integer.
+ *
+ * Return: The total number of characters printed, which includes the '-' sign
+ *         for negative integers.
+ */
+
+
+int _putchar_int(int n)
+{
+	int count = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		count++;
+	}
+	if (n / 10)
+	{
+		count += _putchar_int(n / 10);
+	}
+	_putchar(n % 10 + '0');
+	return (count + 1);
 }
