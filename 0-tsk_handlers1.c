30419c74196ca2ad3d595603f11636518c9a2b2b
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-19 16:00:44 -0700

    printf

--- 0-tsk_handlers1.c
+++ 0-tsk_handlers1.c
@@ -1,5 +1,42 @@
 #include "main.h"
 /**
+ * _print_b - Prints an unsigned integer in binary representation.
+ * @arlist: A va_list containing the unsigned integer to print.
+ *
+ * This function takes an unsigned integer from a va_list and prints its binary
+ * representation to the standard output.
+ *
+ * Return: The number of characters printed.
+ */
+int _print_b(va_list arlist)
+{
+        unsigned int num = va_arg(arlist, unsigned int);
+        int count = 0;
+        int significantDigitFound = 0;
+        int i;
+
+        for (i = sizeof(num) * 8 - 1; i >= 0; i--)
+        {
+                int bit = (num >> i) & 1;
+
+                if (bit == 1 || significantDigitFound)
+                {
+                        _putchar(bit + '0');
+                        count++;
+                        significantDigitFound = 1;
+                }
+        }
+
+        if (count == 0)
+        {
+                _putchar('0');
+                count++;
+        }
+
+        return (count);
+}
+
+/**
  * _print_u - Print an unsigned decimal integer.
  * @arlist: A va_list containing the integer to print.
  *
